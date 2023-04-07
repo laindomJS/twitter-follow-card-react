c442c1689fb7b1b7c15d24a6c85f0391f93ce7bc
laindomJS <adrijar21@gmail.com>
2023-04-07 16:39:56 -0400

    added change text

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,27 +2,26 @@ import { useState } from 'react'
 export const User = (user) => {
   const [isFollowing, setIsFollowing] = useState(false)
 
-  const handleClick = (evt) => {
-    if (isFollowing) {
-      setIsFollowing(false)
-      evt.target.classList.remove('following')
-    }
-    if (!isFollowing) {
-      setIsFollowing(true)
-      evt.target.classList.add('following')
-    }
-  }
+  const handleClick = () => setIsFollowing(!isFollowing)
+  const changeClass = isFollowing ? 'following' : ''
 
   return (
     <div className='user-container'>
       <div className='user-info'>
         <img src={user.avatar} alt={user.username} />
         <div className='user-names'>
-          <strong className='name'>{user.first_name} {user.last_name}</strong>
-          <span className='username'>{user.username}</span>
+          <strong className='name'>
+            {user.first_name} {user.last_name}
+          </strong>
+          <span className='username'>
+            {user.username}
+          </span>
         </div>
       </div>
-      <button onClick={handleClick}>{isFollowing ? 'Siguiendo' : 'Seguir'}</button>
+      <button className={changeClass} onClick={handleClick}>
+        <span className='button-text'>{isFollowing ? 'Siguiendo' : 'Seguir'}</span>
+        <span className='nofollowing'>Dejar de seguir</span>
+      </button>
     </div>
   )
 }
